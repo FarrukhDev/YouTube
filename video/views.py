2804422d97fcb88c34de6df0f9e3edf192442418
Farrux <farruxbekergashaliev@gmail.com>
2022-03-12 23:17:16 +0500

    commit one

--- video/views.py
+++ video/views.py
@@ -9,7 +9,7 @@ from rest_framework.filters import SearchFilter
 class VideoListCreateAPIView(ListCreateAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    searching_fields = ['__all__']
+    search_fields = ['__all__']
 
 class CommentListCreateAPIView(ListCreateAPIView):
     queryset = Comment.objects.all()
@@ -18,19 +18,19 @@ class CommentListCreateAPIView(ListCreateAPIView):
 class PlaylistListCreateAPIView(ListCreateAPIView):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
-    searching_fields = ['__all__']
+    search_fields = ['__all__']
 
 class VideoRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
-    searching_fields = ['title','id']
+    search_fields = ['title','id']
 
 class CommentRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    searching_fields = ['title','id']
+    search_fields = ['title','id']
 
 class PlaylistRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = Playlist.objects.all()
     serializer_class = PlaylistSerializer
-    searching_fields = ['title','id']
+    search_fields = ['title','id']
